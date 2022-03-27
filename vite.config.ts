dfe795569c4e6529fb7aecab230f368533e63896
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-03-27 20:13:53 +0200

    chore: add plugins to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  resolve: {
+    alias: {
+      '~': resolve('src'),
+    },
+    dedupe: ['vue'],
+    extensions: ['.ts', '.vue'],
+  },
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue', '@vueuse/core'],
+      dts: 'src/auto-imports.d.ts',
+    }),
+    WindiCSS(),
+  ],
 })
